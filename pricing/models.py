7a33939422001263143f20d04f39f959ec9758e0
Abdel Bailoul <ab.bailoul@gmail.com>
2024-09-09 12:21:20 +0200

    feat: Create pricing model and make migration

---
+++ pricing/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Pricing(models.Model):
+    name = models.CharField(max_length=50)
+    hourly_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
