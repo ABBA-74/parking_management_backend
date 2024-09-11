958faa9e02c0e6855a66e79fe4d7837d521d0e68
Abdel Bailoul <ab.bailoul@gmail.com>
2024-09-11 18:14:33 +0200

    feat: Add and configure Parking_spot to admin

--- parking_spot/admin.py
+++ parking_spot/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ParkingSpot
 
-# Register your models here.
+class ParkingSpotAdmin(admin.ModelAdmin):
+    list_display = ('id', 'spot_number', 'is_occupied', 'user_id', 'created_at', 'updated_at')
+    list_filter = ('is_occupied', 'user')
+    search_fields = ('spot_number', 'user__username')
+
+admin.site.register(ParkingSpot, ParkingSpotAdmin)
