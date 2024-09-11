13fb7c8861092b15a42435eaab60ce76a44a3677
Abdel Bailoul <ab.bailoul@gmail.com>
2024-09-11 17:38:19 +0200

    feat: Set swagger as the default homepage

--- parking_management/urls.py
+++ parking_management/urls.py
@@ -45,9 +45,8 @@ router.registry.extend(user_management_router.registry)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('', include(router.urls)),
     path('auth/', include('dj_rest_auth.urls')),
     path('api/', include('pricing.urls')),
     path('api/', include('parking_spot.urls')),
