753e52d2917d339489e76a34edda652176222808
Abdel Bailoul <ab.bailoul@gmail.com>
2024-09-11 18:21:24 +0200

    feat: Add and configure Pricing to admin

--- pricing/admin.py
+++ pricing/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Pricing
 
-# Register your models here.
+class PricingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'hourly_rate', 'start_time', 'end_time', 'user_id', 'created_at', 'updated_at')
+    list_filter = ('start_time', 'end_time', 'user')
+    search_fields = ('name', 'user__username')
+
+    # Interdire la suppression des lignes avec 'Jour' et 'Nuit'
+    def has_delete_permission(self, request, obj=None):
+        if obj and obj.name in ['Jour', 'Nuit']:
+            return False
+        return super().has_delete_permission(request, obj)
+
+admin.site.register(Pricing, PricingAdmin)
