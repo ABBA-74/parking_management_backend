588024261d837cf2727451dbfce61b1530d9ed51
Abdel Bailoul <ab.bailoul@gmail.com>
2024-09-11 17:27:34 +0200

    feat: Update History model by changing  onDelete action behavior on ticket foreign key

--- history/models.py
+++ history/models.py
@@ -6,7 +6,7 @@ class History(models.Model):
     event_type = models.CharField(max_length=50)  # Exemple: "Modification", "Paiement", "Annulation"
     description = models.TextField()
     recorded_at = models.DateTimeField(default=timezone.now)
-    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Ticket, on_delete=models.SET_NULL, null=True, blank=True)
     
     def __str__(self):
         return f'{self.event_type} - {self.ticket}'
